0a196f1411f613d8ad10064fe928c19b062e309f
Roberto Fernandes <robertofernandes199@hotmail.com>
2023-02-08 22:19:56 -0300

    update add Temp

--- script.js
+++ script.js
@@ -15,4 +15,42 @@ function carrossel(){
 
 }
 
-setInterval(carrossel, 2000);
+setInterval(carrossel, 3000);
+
+let chave = "87a1f554ca11508e5bec51d0244994a3"
+
+function screen(dados) {
+    //Math.floor é responsavel por arredondar o numero
+    document.querySelector(".city").innerHTML = "Tempo em  " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".umi").innerHTML = dados.main.humidity + "%"
+    //vai montar a url do icone da nuven
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+
+
+
+    console.log(dados)
+}
+
+/*função buscar vai no servidor gratuito e busca as informações */
+async function search(city) {
+    let dados = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q="
+        + city + "&appid="
+        + chave + "&lang=pt_br"
+        + "&units=metric"
+    ).then(res => res.json());
+
+        screen(dados)
+       
+}
+
+/*captura o input e coloca na variavel */
+function clickButton() {
+    let city = document.querySelector(".ctext").value
+    search(city)
+}
+
+
+
